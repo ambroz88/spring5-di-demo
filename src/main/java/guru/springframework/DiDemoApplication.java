5c94fb80f1e95d51395c0e527a0e6b700896ce28
Jiri Ambroz <ambroz88@seznam.cz>
2020-05-06 13:49:30 +0200

    loading properties through java bean

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,9 +1,8 @@
 package guru.springframework;
 
-import guru.springframework.controllers.ConstructorInjectedController;
-import guru.springframework.controllers.GetterInjectedController;
 import guru.springframework.controllers.MyController;
-import guru.springframework.controllers.PropertyInjectedController;
+import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJMSBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,9 +15,13 @@ public class DiDemoApplication {
 
 		MyController controller = (MyController) ctx.getBean("myController");
 
-		System.out.println(controller.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+
+		System.out.println(fakeDataSource.getUser());
+		System.out.println(fakeDataSource.getPassword());
+		System.out.println(fakeDataSource.getUrl());
+
+		FakeJMSBroker fakeJMSBroker = ctx.getBean(FakeJMSBroker.class);
+		System.out.println(fakeJMSBroker.getUsername());
 	}
 }
---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,48 @@
+package guru.springframework.config;
+
+import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJMSBroker;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PropertyConfig {
+
+    @Value("${guru.username}")
+    String user;
+
+    @Value("${guru.password}")
+    String password;
+
+    @Value("${guru.dburl}")
+    String url;
+
+    @Value("${guru.jms.username}")
+    String jmsUsername;
+
+    @Value("${guru.jms.password}")
+    String jmsPassoword;
+
+    @Value("${guru.jms.url}")
+    String jmsUrl;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJMSBroker fakeJmsBroker() {
+        FakeJMSBroker jmsBroker = new FakeJMSBroker();
+        jmsBroker.setUsername(jmsUsername);
+        jmsBroker.setPassword(jmsPassoword);
+        jmsBroker.setUrl(jmsUrl);
+        return jmsBroker;
+    }
+
+}
